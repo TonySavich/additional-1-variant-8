e1af6ddc7eb82c32426af773daf5e73b4e49783f
TonySavich <92541859+TonySavich@users.noreply.github.com>
2022-01-28 21:30:08 +0200

    Add files via upload

---
+++ addLab1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+const int GG = 1000;
+
+
+
+int ms(int* l, int n) {
+    int r;
+    if (n == 0) {
+        return(l[n]);
+  }
+    if (n >=1) {
+        r = l[n] + ms(l,n - 1);
+        return(r);
+    }
+    if (n < 0) {
+        return(0);
+    }
+}
+
+
+int main()
+{
+    int mas[GG];
+    int N;
+    std::cin >> N;
+    for (int i = 0; i < N; i++) {
+        std::cin >> mas[i];
+    }
+    
+    std::cout << ms(mas, N-1)/N; 
+}
+
+
